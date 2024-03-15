304d1df66c7158d723e74a84baff2237616c8952
DemetrioPadre <demetriopadre@gmail.com>
2024-03-15 22:22:49 +0100

    add logo finally

---
+++ resources/views/Collectibles.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.app')
+
+
+@section('main-content')
+<h1>Collectibles</h1>
+@endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,15 +1,40 @@
-<header>
+<header class="container d-flex justify-content-center">
+
+
+
     <ul>
-        <li><a href="{{ route('home') }}">Homepage</a></li>
-        <li><a href="{{ route('Characters') }}">Characters</a></li>
-        <li><a href="{{ route('Comics') }}">Comics</a></li>
-        <li><a href="{{ route('Movies') }}">Movies</a></li>
-        <li><a href="{{ route('Tv') }}">Tv</a></li>
-        <li><a href="{{ route('Games') }}">Games</a></li>
-        <li><a href="{{ route('Collectibles') }}">Collectibles</a></li>
-        <li><a href="{{ route('Video') }}">Video</a></li>
-        <li><a href="{{ route('Fans') }}">Fans</a></li>
-        <li><a href="{{ route('News') }}">News</a></li>
-        <li><a href="{{ route('Shop') }}">Shop</a></li>
+        <li>
+            <a @class([ 'link' , 'active'=> Route::currentRouteName()=='home' ]) href="{{ route('home') }}"> <img src="{{Vite::asset('/resources/images/dc-logo.png')}}" alt=""></a>
+        </li>
+        <li>
+            <a @class([ 'link' , 'active'=> Route::currentRouteName()=='Characters' ]) href="{{ route('Characters') }}">Characters</a>
+        </li>
+        <li>
+            <a @class([ 'link' , 'active'=> Route::currentRouteName()=='Comics' ])href="{{ route('Comics') }}">Comics</a>
+        </li>
+        <li>
+            <a @class([ 'link' , 'active'=> Route::currentRouteName()=='Movies' ]) href="{{ route('Movies') }}">Movies</a>
+        </li>
+        <li>
+            <a @class([ 'link' , 'active'=> Route::currentRouteName()=='Tv' ])href="{{ route('Tv') }}">Tv</a>
+        </li>
+        <li>
+            <a @class([ 'link' , 'active'=> Route::currentRouteName()=='Games' ])href="{{ route('Games') }}">Games</a>
+        </li>
+        <li>
+            <a @class([ 'link' , 'active'=> Route::currentRouteName()=='Collectibles' ]) href="{{ route('Collectibles') }}">Collectibles</a>
+        </li>
+        <li>
+            <a @class([ 'link' , 'active'=> Route::currentRouteName()=='Video' ]) href="{{ route('Video') }}">Video</a>
+        </li>
+        <li>
+            <a @class([ 'link' , 'active'=> Route::currentRouteName()=='Fans' ]) href="{{ route('Fans') }}">Fans</a>
+        </li>
+        <li>
+            <a @class([ 'link' , 'active'=> Route::currentRouteName()=='News' ]) href="{{ route('News') }}">News</a>
+        </li>
+        <li>
+            <a @class([ 'link' , 'active'=> Route::currentRouteName()=='Shop' ]) href="{{ route('Shop') }}">Shop</a>
+        </li>
     </ul>
 </header>

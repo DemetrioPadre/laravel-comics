e50bd231237ec3f369f6684ed3b33b213f5b8c89
DemetrioPadre <demetriopadre@gmail.com>
2024-03-18 20:01:41 +0100

    add parts of footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -24,21 +24,53 @@
 <div class="backgrnd">
 
     <div class="row ">
-        <div class="col-6">
-            <div class="middleFoot">
-                <div class="linkF ">
-                    <p>DC COMICS</p>
+        <div class="col-6 d-flex">
+            <div class="linkF mt-5 mx-3">
+                <p class="mx-4">DC COMICS</p>
+                <ul>
+                    <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>TV</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li>News</li>
+                </ul>
+                <div class="linkS">
+                    <p class="mx-4">SHOP</p>
                     <ul>
-                        <li>Characters</li>
-                        <li>Comics</li>
-                        <li>Movies</li>
-                        <li>TV</li>
-                        <li>Games</li>
-                        <li>Videos</li>
-                        <li>News</li>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
                     </ul>
                 </div>
             </div>
+            <div class="linkT  mt-5">
+                <p class="mx-4">DC</p>
+                <ul>
+                    <li>Terms Of Use</li>
+                    <li>Privacy policy(New)</li>
+                    <li>Ad Choise</li>
+                    <li>Adversiting</li>
+                    <li>Jobs</li>
+                    <li>Subscriptions</li>
+                    <li>Talent Workshops</li>
+                    <li>CPSC Certificates</li>
+                    <li>Rating</li>
+                    <li>Shop Help</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+            <div class="linkF mt-5">
+                <p class="mx-4">Sites</p>
+                <ul>
+                    <li>DC</li>
+                    <li>MAD Magazine</li>
+                    <li>DC Kids</li>
+                    <li>DC Universe</li>
+                    <li>DC Power Visa </li>
+                </ul>
+            </div>
+
         </div>
 
         <div class="col-6">

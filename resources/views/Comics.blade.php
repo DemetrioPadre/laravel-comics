4176b50f6f224fa60215287642a72ecf9f089d38
DemetrioPadre <demetriopadre@gmail.com>
2024-03-18 07:45:48 +0100

    add footer struct

--- resources/views/Comics.blade.php
+++ resources/views/Comics.blade.php
@@ -4,9 +4,10 @@
 @section('main-content')
 <h1>Fumetti</h1>
 <div class="row">@foreach($Comics as $Comic )
-    <div class="col-3">
+    <div class="col-2">
         <div class="card">
             <img src="{{$Comic['thumb']}}" class="card-img-top" alt="{{$Comic['series']}}">
+            <p>{{$Comic['title']}}</p>
         </div>
     </div>
 
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,21 @@
-<footer>footer</footer>
+<footer class="container d-flex justify-content-center mt-4">
+    <div class="upperfooter container d-flex justify-content-center mt-4">
+        <ul>
+            <li>
+                <a @class([ 'link' , 'active'=> Route::currentRouteName()=='home' ]) href="{{ route('home') }}"></a>
+            </li>
+            <li>
+                <a @class([ 'link' , 'active'=> Route::currentRouteName()=='home' ]) href="{{ route('home') }}"></a>
+            </li>
+            <li>
+                <a @class([ 'link' , 'active'=> Route::currentRouteName()=='home' ]) href="{{ route('home') }}"></a>
+            </li>
+            <li>
+                <a @class([ 'link' , 'active'=> Route::currentRouteName()=='home' ]) href="{{ route('home') }}"></a>
+            </li>
+            <li>
+                <a @class([ 'link' , 'active'=> Route::currentRouteName()=='home' ]) href="{{ route('home') }}"></a>
+            </li>
+        </ul>
+    </div>
+</footer>
